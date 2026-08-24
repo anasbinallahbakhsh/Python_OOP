a57a31de4da247980afad82c83b6fadfbe601e51
Muhammad Anas <anaskhan321246@gmail.com>
2026-08-23 20:48:50 -0700

    solution

---
+++ OOP_EXERCISE_3_solution.py
@@ -0,0 +1,15 @@
+class laptop:
+    def __init__(self,brand,model_name,price):
+        self.brand=brand
+        self.model_name=model_name
+        self.price=price
+        self.laptop_name=brand + "",model_name+""
+    def apply_discount(self,num):
+        # self.price
+       off_price=(num/100)*self.price
+       return self.price - off_price
+laptop5=laptop('dell','core i 6', 8000)
+
+laptop2=laptop('apple','macbook', 20000)
+
+print(laptop2.apply_discount(50))
