74e70d4ac1f1685fac8afe3cfefad3b3f17be3dd
Muhammad Anas <anaskhan321246@gmail.com>
2026-08-28 18:52:42 -0700

    practic

---
+++ EXercise_7_.py
@@ -0,0 +1,6 @@
+class person:
+    def __init__(self,first_name,last_name):
+        self.first_name=first_name
+        self.last_name=last_name
+anas_person=person('anas', 'malik')
+print(anas_person.first_name)
